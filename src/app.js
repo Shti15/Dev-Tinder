b517834d7b1415e92f72f1f85568a3489e616082
Shreya Tiwari <shreyatiwari1505@gmail.com>
2024-12-06 11:49:50 +0530

    get post delete use

--- src/app.js
+++ src/app.js
@@ -4,6 +4,17 @@ const express=require('express');
 const app=express();
 
 app.listen(9988);
+
+app.get("/user",(req,res)=>{
+    res.send({firstname:"shreya",lastname:"tiwari"})
+})
+app.post("/user",(req,res)=>{
+    res.send("data saved successfully!!!")
+})
+app.delete("/user",(req,res)=>{
+    res.send("data deleted successfully")
+})
+
 app.use("/test",(req,res)=>{
     res.send("test page!!!")
 })
